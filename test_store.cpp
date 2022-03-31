3d766d8f4a21f1aca16a94e5ce60bb7ae9573bc4
Yichao Yu <yyc1992@gmail.com>
2022-03-31 14:37:46 -0400

    Normal load/store test

---
+++ test_store.cpp
@@ -0,0 +1,16 @@
+//
+
+#include "pmu_utils.h"
+
+#include <stdlib.h>
+
+static volatile int g = 0;
+
+int main(int argc, char **argv)
+{
+    perf_res([&] (int n) {
+        for (int i = 0; i < n; i++) {
+            g = i;
+        }
+    }, atoi(argv[1]), atoi(argv[2])).print();
+}
