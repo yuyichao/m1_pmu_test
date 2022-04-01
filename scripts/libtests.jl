15f289f93b7561b69dc537f519eafa804ebc639d
Yichao Yu <yyc1992@gmail.com>
2022-03-31 20:04:02 -0400

    branch test

--- scripts/libtests.jl
+++ scripts/libtests.jl
@@ -2,14 +2,14 @@
 
 using Random
 
-set_scores(ice, fire) = ccall((:set_cores, "./libpmu_test.so"),
-                              Cvoid, (Cint, Cint), ice, fire)
+set_cores(ice, fire) = ccall((:set_cores, "./libpmu_test.so"),
+                             Cvoid, (Cint, Cint), ice, fire)
 
 function branch_test!(n, randomize, ice_res, fire_res)
     conds = zeros(Int8, n)
-    conds[1:(n ÷ 2)] = 1
+    conds[1:(n ÷ 2)] .= 1
     if randomize
-        randcycle!(conds)
+        shuffle!(conds)
     end
     ccall((:branch_test, "./libpmu_test.so"),
           Cvoid, (Ptr{Int8}, Cint, Ptr{Int64}, Ptr{Int64}),
@@ -19,7 +19,7 @@ end
 function branch_target_test!(n, randomize, ice_res, fire_res)
     tgts = Int8[div((i - 1) * 8, n) for i in 1:n]
     if randomize
-        randcycle!(tgts)
+        shuffle!(tgts)
     end
     ccall((:branch_target_test, "./libpmu_test.so"),
           Cvoid, (Ptr{Int8}, Cint, Ptr{Int64}, Ptr{Int64}),
@@ -29,7 +29,7 @@ end
 function mem_load_test!(sz, n, randomize, ice_res, fire_res)
     buff = Cint[1:(sz - 1); 0]
     if randomize
-        randcycle!(buff)
+        shuffle!(buff)
     end
     ccall((:mem_load_test, "./libpmu_test.so"),
           Cvoid, (Ptr{Cint}, Cint, Ptr{Int64}, Ptr{Int64}),
