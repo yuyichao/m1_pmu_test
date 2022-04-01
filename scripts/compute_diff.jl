916c440f587efccb9946cbaa40b7796bb325ab67
Yichao Yu <yyc1992@gmail.com>
2022-04-01 07:27:48 -0400

    Compute differences

--- scripts/compute_diff.jl
+++ scripts/compute_diff.jl
@@ -28,3 +28,34 @@ function resave_diff(outfile, infile1, infile2)
         end
     end
 end
+
+function find_similar(data1, data2)
+    lines = Vector{Float64}[]
+    for i in 1:size(data1, 1)
+        if abs(data1[i, 3] - data2[i, 3]) >= 0.1 || abs(data1[i, 7] - data2[i, 7]) >= 0.1
+            continue
+        end
+        if data1[i, 3] <= 0.01 && data2[i, 3] <= 0.01 &&
+            data1[i, 7] <= 0.01 && data2[i, 7] <= 0.01
+            continue
+        end
+        if data1[i, 4] == 0 && data1[i, 5] == 0 && data1[i, 8] == 0 && data1[i, 9] == 0 &&
+            data2[i, 4] == 0 && data2[i, 5] == 0 && data2[i, 8] == 0 && data2[i, 9] == 0
+            continue
+        end
+        push!(lines, [data1[i, :]; data2[i, 2:end]])
+    end
+    return lines
+end
+
+function resave_similar(outfile, infile1, infile2)
+    data1 = load_summary(infile1)
+    data2 = load_summary(infile2)
+    similar_lines = find_similar(data1, data2)
+    open(outfile, "w") do fd
+        println(fd, "evt,ice_ma1,ice_mb1,fire_ma1,fire_mb1,ice_ma2,ice_mb2,fire_ma2,fire_mb2")
+        for line in similar_lines
+            println(fd, "$(Int(line[1])),$(join(line[[2,3,6,7,10,11,14,15]], ","))")
+        end
+    end
+end
