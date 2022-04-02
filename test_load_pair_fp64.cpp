d5ea3e483c2f71ea76b428640a053153a5db4e05
Yichao Yu <yyc1992@gmail.com>
2022-04-01 21:47:12 -0400

    Load store pair test

---
+++ test_load_pair_fp64.cpp
@@ -0,0 +1,18 @@
+//
+
+#include "pmu_utils.h"
+
+#include <stdlib.h>
+
+static int g[16] __attribute__((aligned(64)));
+
+int main(int argc, char **argv)
+{
+    perf_res([&] (int n) {
+        for (int i = 0; i < n; i++) {
+            float v1, v2;
+            asm volatile ("ldp %d0, %d1, [%2]\n" :
+                          "=w"(v1), "=w"(v2) : "r"(g) : "memory");
+        }
+    }, atoi(argv[1]), atoi(argv[2])).print();
+}
---
+++ test_store_pair32.cpp
@@ -0,0 +1,18 @@
+//
+
+#include "pmu_utils.h"
+
+#include <stdlib.h>
+
+static int g[16] __attribute__((aligned(64)));
+
+int main(int argc, char **argv)
+{
+    perf_res([&] (int n) {
+        for (int i = 0; i < n; i++) {
+            int v1, v2;
+            asm volatile ("stp %w0, %w1, [%2]\n" :
+                          "=r"(v1), "=r"(v2) : "r"(g) : "memory");
+        }
+    }, atoi(argv[1]), atoi(argv[2])).print();
+}
