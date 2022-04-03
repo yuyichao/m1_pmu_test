563b97937b4fd9485b5c161a02b98eaae1b25c83
Yichao Yu <yyc1992@gmail.com>
2022-04-03 18:52:55 -0400

    Reduce allocation...

--- scripts/runtest_int.jl
+++ scripts/runtest_int.jl
@@ -7,18 +7,16 @@ using Printf
 
 set_cores(0, 4)
 
-function run_loops(f, loops)
+function run_loops!(f, loops, res)
     nloops = length(loops)
-    res = zeros(Int64, 256, length(loops) * 2)
     for i in 1:nloops
         f(loops[i], @view(res[:, i * 2 - 1]), @view(res[:, i * 2]))
     end
-    return res
 end
 
-function run_and_save(f, file)
+function run_and_save!(f, loops, res, file)
     open(file, "w") do fh
-        res = run_loops(f, [1, 100, 10000, 1000000])
+        run_loops!(f, loops, res)
         println(fh, "evt,1,1,100,100,10000,10000,1000000,1000000")
         for i in 1:size(res, 1)
             @printf(fh, "%02x", i - 1)
@@ -30,7 +28,8 @@ function run_and_save(f, file)
     end
 end
 
-function run_all(idx_begin, idx_end)
+function run_all(idx_begin, idx_end, loops)
+    res = zeros(Int64, 256, length(loops) * 2)
     for (key, val) in testgrp_int
         mkpath(joinpath(@__DIR__, "..", "data", "test_$(key)"), mode=0o755)
     end
@@ -40,9 +39,10 @@ function run_all(idx_begin, idx_end)
             if i % 100 == 0
                 println("Running: $key")
             end
-            run_and_save(val, joinpath(@__DIR__, "..", "data", "test_$(key)", "$(i).csv"))
+            run_and_save!(val, loops, res,
+                         joinpath(@__DIR__, "..", "data", "test_$(key)", "$(i).csv"))
         end
     end
 end
 
-run_all(parse(Int, ARGS[1]), parse(Int, ARGS[2]))
+run_all(parse(Int, ARGS[1]), parse(Int, ARGS[2]), [1, 100, 10000, 1000000])
