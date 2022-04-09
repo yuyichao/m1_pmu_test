3a2d3f76ab640dcc30167c15d3a3920fa4b40929
Yichao Yu <yyc1992@gmail.com>
2022-04-08 22:45:16 -0400

    tlb miss test

---
+++ scripts/test_mem4_tlb.jl
@@ -0,0 +1,116 @@
+#!/usr/bin/julia
+
+# Run in the build directory
+include("libtests.jl")
+
+using Printf
+
+set_cores(0, 4)
+
+function run_load_loops(loops, randomize, page, sz)
+    nloops = length(loops)
+    res = zeros(Int64, 256, length(loops) * 2)
+    for i in 1:nloops
+        mem_load_test3!(sz, page ? 16 * 1024 : 1, loops[i], randomize,
+                        @view(res[:, i * 2 - 1]), @view(res[:, i * 2]))
+    end
+    return res
+end
+
+function run_store_loops(loops, randomize, page, sz)
+    nloops = length(loops)
+    res = zeros(Int64, 256, length(loops) * 2)
+    for i in 1:nloops
+        mem_store_test2!(sz, page ? 16 * 1024 : 1, loops[i], randomize,
+                         @view(res[:, i * 2 - 1]), @view(res[:, i * 2]))
+    end
+    return res
+end
+
+function run_load_and_save(file, randomize, page, sz, loops)
+    open(file, "w") do fh
+        res = run_load_loops(loops, randomize, page, sz)
+        println(fh, "evt," * join(["$(l),$(l)" for l in loops], ","))
+        for i in 1:size(res, 1)
+            @printf(fh, "%02x", i - 1)
+            for j in 1:size(res, 2)
+                print(fh, ",$(res[i, j])")
+            end
+            println(fh)
+        end
+    end
+end
+
+function run_store_and_save(file, randomize, page, sz, loops)
+    open(file, "w") do fh
+        res = run_store_loops(loops, randomize, page, sz)
+        println(fh, "evt," * join(["$(l),$(l)" for l in loops], ","))
+        for i in 1:size(res, 1)
+            @printf(fh, "%02x", i - 1)
+            for j in 1:size(res, 2)
+                print(fh, ",$(res[i, j])")
+            end
+            println(fh)
+        end
+    end
+end
+
+function run_load_size(page, sz, i, loops)
+    dir_reg = joinpath(@__DIR__, "..", "data",
+                       "test_mem_load4_$(page ? "page" : "dense")_regular_$sz")
+    dir_rand = joinpath(@__DIR__, "..", "data",
+                        "test_mem_load4_$(page ? "page" : "dense")_randomize_$sz")
+    mkpath(dir_reg, mode=0o755)
+    mkpath(dir_rand, mode=0o755)
+    run_load_and_save(joinpath(dir_reg, "$(i).csv"), false, page, sz, loops)
+    run_load_and_save(joinpath(dir_rand, "$(i).csv"), true, page, sz, loops)
+end
+
+function run_store_size(page, sz, i, loops)
+    dir_reg = joinpath(@__DIR__, "..", "data",
+                       "test_mem_store4_$(page ? "page" : "dense")_regular_$sz")
+    dir_rand = joinpath(@__DIR__, "..", "data",
+                        "test_mem_store4_$(page ? "page" : "dense")_randomize_$sz")
+    mkpath(dir_reg, mode=0o755)
+    mkpath(dir_rand, mode=0o755)
+    run_store_and_save(joinpath(dir_reg, "$(i).csv"), false, page, sz, loops)
+    run_store_and_save(joinpath(dir_rand, "$(i).csv"), true, page, sz, loops)
+end
+
+function run_store_size(sz, i, loops)
+    dir_reg = joinpath(@__DIR__, "..", "data", "test_mem_store3_regular_$sz")
+    dir_rand = joinpath(@__DIR__, "..", "data", "test_mem_store3_randomize_$sz")
+    mkpath(dir_reg, mode=0o755)
+    mkpath(dir_rand, mode=0o755)
+    run_store_and_save(joinpath(dir_reg, "$(i).csv"), false, sz, loops)
+    run_store_and_save(joinpath(dir_rand, "$(i).csv"), true, sz, loops)
+end
+
+const loops = [1, 1000, 30_000, 1000_000]
+
+function run_all_sizes(idx, sizes)
+    for sz in sizes
+        if idx % 100 == 0
+            println(sz)
+            @time run_load_size(true, sz, idx, loops)
+            @time run_store_size(true, sz, idx, loops)
+            @time run_load_size(false, sz, idx, loops)
+            @time run_store_size(false, sz, idx, loops)
+        else
+            run_load_size(true, sz, idx, loops)
+            run_store_size(true, sz, idx, loops)
+            run_load_size(false, sz, idx, loops)
+            run_store_size(false, sz, idx, loops)
+        end
+    end
+end
+
+function run_all(idx_begin, idx_end)
+    sizes = 32 .* [3, 4, 5, 6, 8, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256]
+    for i in idx_begin:idx_end
+        println("Index: $i")
+        @time run_all_sizes(i, sizes)
+    end
+end
+
+run_all(parse(Int, ARGS[1]), parse(Int, ARGS[2]))
