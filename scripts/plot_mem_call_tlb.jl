f6ca0960b3e96740a02191ba081029fc743e441a
Yichao Yu <yyc1992@gmail.com>
2022-04-11 08:39:16 -0400

    More zoomed in plots

--- scripts/plot_mem_call_tlb.jl
+++ scripts/plot_mem_call_tlb.jl
@@ -42,6 +42,17 @@ function check_diff(diff)
     return true
 end
 
+function check_diff2(diff)
+    if all(x->abs(x) <= 0.06, diff)
+        return false
+    elseif any(x->x < -0.04, diff)
+        return false
+    elseif any(x->x > 2, diff)
+        return false
+    end
+    return true
+end
+
 function resort_data(diff)
     sizes = sort!(collect(keys(diff)))
     ice_evt_diff = Dict(i=>Float64[] for i in 0:255)
@@ -107,6 +118,49 @@ function plot_all_diff(name, sizes, ice_evt_diff, fire_evt_diff)
     tight_layout()
 end
 
+function plot_all_diff2(name, sizes, ice_evt_diff, fire_evt_diff)
+    figure(figsize=[12.6, 5.6])
+    subplot(1, 2, 1)
+    ice_idx = 0
+    for evt in 0:255
+        diff = ice_evt_diff[evt]
+        if !check_diff2(diff)
+            continue
+        end
+        ice_idx += 1
+        plot(sizes, diff, get_fmt(ice_idx), label="$(evt)")
+    end
+    axvline(128, color="r")
+    axvline(1024, color="g")
+    gca()[:set_xscale]("log")
+    xlim([64, 32 * 256])
+    ylim([0, ylim()[2]])
+    xlabel("Pages")
+    ylabel("Count Difference")
+    legend(ncol=6, fontsize=8)
+    title("Ice Storm ($name)")
+    subplot(1, 2, 2)
+    fire_idx = 0
+    for evt in 0:255
+        diff = fire_evt_diff[evt]
+        if !check_diff2(diff)
+            continue
+        end
+        fire_idx += 1
+        plot(sizes, diff, get_fmt(fire_idx), label="$(evt)")
+    end
+    axvline(160, color="r")
+    axvline(3072, color="g")
+    gca()[:set_xscale]("log")
+    xlim([64, 32 * 256])
+    ylim([0, ylim()[2]])
+    xlabel("Page Count")
+    ylabel("Count Difference")
+    legend(ncol=6, fontsize=8)
+    title("Fire Storm ($name)")
+    tight_layout()
+end
+
 const raw_data = load_all(joinpath(@__DIR__, "../data"))
 const diff_data = compute_diff(raw_data)
 const resorted_data = resort_data(diff_data)
@@ -115,5 +169,8 @@ const prefix = joinpath(@__DIR__, "../imgs/mem_call_tlb")
 
 plot_all_diff("Call", resorted_data...)
 NaCsPlot.maybe_save("$(prefix)_all")
+
+plot_all_diff2("Call", resorted_data...)
+NaCsPlot.maybe_save("$(prefix)_small")
 
 NaCsPlot.maybe_show()
